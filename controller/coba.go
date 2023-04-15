cfbb27f4411de505ab242e54a883e5c18fb8c01f
Farel Nouval <fareldazz123@gmail.com>
2023-04-15 20:43:25 +0700

    up up

--- controller/coba.go
+++ controller/coba.go
@@ -1,11 +1,18 @@
 package controller
 
 import (
+	"errors"
+	"fmt"
+	"net/http"
+
 	"github.com/FarelND29/farel/config"
 	inimodul "github.com/FarelND29/monitoring_orang_tua/module"
 	"github.com/aiteung/musik"
 	cek "github.com/aiteung/presensi"
 	"github.com/gofiber/fiber/v2"
+	inimodullatihan "github.com/indrariksa/be_presensi/module"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func Homepage(c *fiber.Ctx) error {
@@ -15,6 +22,42 @@ func Homepage(c *fiber.Ctx) error {
 
 func GetPresensi(c *fiber.Ctx) error {
 	ps := cek.GetPresensiCurrentMonth(config.Ulbimongoconn)
+	return c.JSON(ps)
+}
+
+func GetAllPresensi(c *fiber.Ctx) error {
+	ps := inimodullatihan.GetAllPresensi(config.Ulbimongoconn2, "presensi")
+	return c.JSON(ps)
+}
+
+func GetPresensiID(c *fiber.Ctx) error {
+	id := c.Params("id")
+	if id == "" {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"status":  http.StatusInternalServerError,
+			"message": "Wrong parameter",
+		})
+	}
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Map{
+			"status":  http.StatusBadRequest,
+			"message": "Invalid id parameter",
+		})
+	}
+	ps, err := inimodullatihan.GetPresensiFromID(objID, config.Ulbimongoconn2, "presensi")
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return c.Status(http.StatusNotFound).JSON(fiber.Map{
+				"status":  http.StatusNotFound,
+				"message": fmt.Sprintf("No data found for id %s", id),
+			})
+		}
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"status":  http.StatusInternalServerError,
+			"message": fmt.Sprintf("Error retrieving data for id %s", id),
+		})
+	}
 	return c.JSON(ps)
 }
 
