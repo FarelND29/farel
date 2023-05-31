35a761c3483af2f02593504187527a5716088744
Farel Nouval <fareldazz123@gmail.com>
2023-05-31 14:28:37 +0700

    week11

--- main.go
+++ main.go
@@ -17,6 +17,18 @@ import (
 	_ "github.com/FarelND29/farel/docs"
 )
 
+// @title Fiber Example API
+// @version 1.0
+// @description This is a sample swagger for Fiber
+
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email fiber@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8080
+// @BasePath /
+
 func main() {
 	go whatsauth.RunHub()
 	site := fiber.New(config.Iteung)
--- url/url.go
+++ url/url.go
@@ -8,6 +8,8 @@ import (
 	"github.com/gofiber/websocket/v2"
 )
 
+
+
 func Web(page *fiber.App) {
 	page.Post("/api/whatsauth/request", controller.PostWhatsAuthRequest)  //API from user whatsapp message from iteung gowa
 	page.Get("/ws/whatsauth/qr", websocket.New(controller.WsWhatsAuthQR)) //websocket whatsauth
